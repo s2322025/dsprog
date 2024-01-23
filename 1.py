d8bdbd600dcbf5fce6ef7af29d1a234f0122ae4b
s2322025 <s2322025@stu.musashino-u.ac.jp>
2024-01-23 17:09:33 +0900

    ちょっと修正

--- 1.py
+++ 1.py
@@ -2,13 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import sqlite3
 
-dbname = "konzatu"
-conn = sqlite3.connect(dbname)
-cur = conn.cursor()
-create_table = "create table konzatu (id text,name text)"
-cur.execute(create_table)
-
-conn.close()
 
 
 url = requests.get('https://www.navitime.co.jp/congestion/prediction/result?node=00004538&from=congestion.prediction.railroad')
@@ -21,3 +14,12 @@ for e in soup.find_all('dt',class_ = "week-date-text"):
 konzatu = []
 for e in soup.find_all('span',class_ = "blue week-status-text"):
     konzatu.append(e.getText().replace("\n", ""))
+
+
+dbname = "konzatu"
+conn = sqlite3.connect(dbname)
+cur = conn.cursor()
+create_table = "create table if not exists konzatu (date text,congestion text)"
+cur.execute(create_table)
+conn.commit()
+conn.close()
