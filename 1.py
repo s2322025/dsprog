2aa2b5364d2a0bbc491a90aec20afb21b2b78ae9
s2322025 <s2322025@stu.musashino-u.ac.jp>
2024-01-23 16:09:46 +0900

    日にちのみを取得

--- 1.py
+++ 1.py
@@ -2,5 +2,5 @@ import requests
 from bs4 import BeautifulSoup
 url = requests.get('https://www.navitime.co.jp/congestion/prediction/result?node=00004538&from=congestion.prediction.railroad')
 soup = BeautifulSoup(url.text, "html.parser")
-for e in soup.find_all('dt'):
+for e in soup.find_all('dt',class_= "week-date-text"):
     print(e.getText())
