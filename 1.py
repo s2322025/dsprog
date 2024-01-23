c42f28cc0817718ead9ac3dd8799f9a0d9329cd5
s2322025 <s2322025@stu.musashino-u.ac.jp>
2024-01-23 16:07:36 +0900

    dtタグのテキストを取得

--- 1.py
+++ 1.py
@@ -2,3 +2,5 @@ import requests
 from bs4 import BeautifulSoup
 url = requests.get('https://www.navitime.co.jp/congestion/prediction/result?node=00004538&from=congestion.prediction.railroad')
 soup = BeautifulSoup(url.text, "html.parser")
+for e in soup.find_all('dt'):
+    print(e.getText())
