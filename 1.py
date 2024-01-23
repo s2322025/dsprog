dd13899e6e9576b68f9efe6d0fed7fa2bf399a7c
s2322025 <s2322025@stu.musashino-u.ac.jp>
2024-01-23 16:30:05 +0900

    DBを作成

--- 1.py
+++ 1.py
@@ -1,5 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+
+dbname = "konzatu"
+conn = sqlite3.connect(dbname)
+
+conn.close()
+
+
 url = requests.get('https://www.navitime.co.jp/congestion/prediction/result?node=00004538&from=congestion.prediction.railroad')
 soup = BeautifulSoup(url.text, "html.parser")
 
@@ -10,6 +18,3 @@ for e in soup.find_all('dt',class_ = "week-date-text"):
 konzatu = []
 for e in soup.find_all('span',class_ = "blue week-status-text"):
     konzatu.append(e.getText().replace("\n", ""))
-
-for i in range(5):
-    print(konzatu_date[i],konzatu[i])
